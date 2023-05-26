36b1b4b1a041b8164b85461473e00ff287060d6b
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-05-25 22:25:55 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const handleSearch = async (event) => {
   event.preventDefault();
-//em cima deixa
+//Aluno
 
-  const espacoMensagem = document.getElementById("mensagem");
-  const espacoFoto = document.getElementById("shows");
+  const menssagem = document.getElementById("message");
+  const conteudo = document.getElementById("shows");
   const entradaTexto = document.getElementById("query");
 
   const endpoint = `https://api.tvmaze.com/search/shows?q=${entradaTexto.value}`;
   const resposta = await fetch(endpoint);
+  const show = await resposta.json();
+
+  //conteúdo que o usuário escreveu
+
+if(show.lenght === 0){
+  menssagem.innerHTML = 'Busca não encontrada...';  
+  return;
+}
+
+menssagem.innerHTML = '';
+
+show.forEach((entry) => {
+  conteudo.insertAdjacentHTML(
+    'beforeEnd',
+    `<li>
+    <img class="poster" src='${entry?.show?.image?.medium || ''}  /> 
+    <span class="show-name">${entry?.show?.name || ''}</span> 
+    </li>`)
+  });
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+//Professor
 
   //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
 
   //// Elementos de leiaute importantes:
 
   //  #message: use para exibir mensagens aos usuário, por exemplo:
-  
-  const erro = document.querySelector('#message');
-  erro.innerHTML = '<img id="erro" src="https://preview.redd.it/7fcu41hi2re41.png?auto=webp&s=038dc69c1622eb809ebbfbc15c22c5f6739dca65" alt="o gato Tom dando os ombros">';
-  
   
   //  #shows: conterá os shows, cada um em um <li>, por exemplo:
   // <li>
